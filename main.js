9ce641b5f7c9208524659d52f63805668ffe459b
Fábio Zabini <fabio.002@hotmail.com>
2023-09-25 21:43:47 -0300

    incluso o arquivo JS

---
+++ main.js
@@ -0,0 +1,104 @@
+const form = document.getElementById("meuForm");
+const button = document.getElementById("button");
+
+form.addEventListener("input", function () {
+  if (form.checkValidity()) {
+    button.removeAttribute("disabled");
+  } else {
+    button.setAttribute("disabled", "disabled");
+  }
+});
+
+const formulario = document.getElementById("meuForm");
+const emailInput = document.getElementById("email");
+const errorMensage = document.getElementById("errormensage");
+emailInput.addEventListener("input", function () {
+  const value = emailInput.value;
+  const result = isValidEmail(value);
+  console.log(result);
+  if (!result) {
+    errorMensage.textContent = "Campo inválido";
+    errorMensage.style.color = "red";
+    errorMensage.style.fontWeight = "bold";
+  } else {
+    errorMensage.textContent = "";
+  }
+});
+
+function isValidEmail(email) {
+  const emailRegex = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/;
+  return emailRegex.test(email);
+}
+
+
+
+const telefoneInput = document.getElementById("telefone");
+const error = document.getElementById("error");
+telefoneInput.addEventListener("input", function () {
+  let value = telefoneInput.value;
+  value = value.replace(/\D/g, '');
+
+  if (value.length >= 2 ) {
+    value = '(' + value.substr(0, 2) + ') ' + value.substr(2);
+  } 
+ 
+  if(value.length > 9) {
+    value = value.substr(0, 10) + '-' + value.substr(10);
+  }
+
+  //  Fabio se nao der certo o problema de apagar tenta esse codigo de baixo...
+
+  
+  // if (value.length <= 2) {
+  //   value = '(' + value;
+  // } else if (value.length > 2 && value.length <= 9) {
+  //   value = '(' + value.substring(0, 2) + ') ' + value.substring(2);
+  // } else if (value.length > 9) {
+  //   value = '(' + value.substring(0, 2) + ') ' + value.substring(2, 9) + '-' + value.substring(9);
+  // }
+
+  telefoneInput.value = value;
+  console.log(telefoneInput.value);
+  const resultado = isValidTelefone(value);
+  
+  if (!resultado) {
+    error.textContent = "Campo inválido";
+    error.style.color = "red";
+    error.style.fontWeight = "bold";
+  } else {
+    error.textContent = "";
+  }
+});
+
+function isValidTelefone(telefone) {
+  const telefoneRegex = /^\(\d{2}\) \d{4,5}-\d{4}$/;
+  return telefoneRegex.test(telefone);
+}
+
+const cepInput = document.getElementById("cep");
+const errormsg = document.getElementById("errormsg");
+cepInput.addEventListener("input", function () {
+  const value = cepInput.value;
+  const result = isValidCep(value);
+  console.log(result);
+  if (!result) {
+    errormsg.textContent = "Campo inválido";
+    errormsg.style.color = "red";
+    errormsg.style.fontWeight = "bold";
+  } else {
+    errormsg.textContent = "";
+  }
+});
+
+function isValidCep(cep) {
+  const cepRegex = /^\d{5}-\d{3}$/;
+  return cepRegex.test(cep);
+}
+
+
+// function isValidTelefone(telefone) {
+//   const telefoneRegex = /^\(\d{2}\) \d{4,5}-\d{4}$/;
+//   return telefoneRegex.test(telefone);
+// }
+
+
